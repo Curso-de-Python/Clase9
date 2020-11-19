5fcfabf7e947d21e36edd59ca80f1bb65bdaa84c
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-11-19 01:40:30 -0500

    Create ejercicio6.py

---
+++ ejercicio6.py
@@ -0,0 +1,23 @@
+'''
+-----------------------------
+ EJERCICIO N°6
+ Módulo platform
+-----------------------------
+'''
+from platform import *
+
+print(platform())
+print(platform(1))
+print(platform(0, 1))
+
+#print(machine())
+
+#print(processor())
+
+#print(system())
+
+#print(version())
+
+#print(python_implementation())
+
+#print(python_version_tuple())
