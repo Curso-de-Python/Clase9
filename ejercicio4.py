40263ded9a38834e6ba5f0dc40b49651ffdf01f9
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-11-19 01:38:35 -0500

    Create ejercicio4.py

---
+++ ejercicio4.py
@@ -0,0 +1,32 @@
+'''
+-----------------------------
+ EJERCICIO N°4
+ Importación de Módulos
+-----------------------------
+'''
+# PRIMER MÉTODO
+import math
+print(math.sin(math.pi/2))
+
+#pi = 3.14
+#print(pi)
+#print(math.pi)
+
+# SEGUNDO MÉTODO
+#from math import sin, pi
+#print(sin(pi/2))
+
+# TERCER MÉTODO (cambiar por import *)
+
+'''
+-----------------------------
+ ALIASING
+-----------------------------
+'''
+# ALIAS PARA EL MÓDULO
+#import math as m
+#print(m.sin(m.pi/2))
+
+# ALIAS PARA LAS ENTIDADES
+#from math import pi as PI, sin as sen
+#print(sen(PI/2))
